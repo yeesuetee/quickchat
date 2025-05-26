4d2d60057e7d0c039cfdeb13f302c1dc1aefc92e
Shannon Yee <yeesuetee@gmail.com>
2025-05-26 15:37:19 +0000

    Environment setup and simple UI

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# quickchat
-Simple chatbot POC
+# QuickChat
+
+**QuickChat** is a simple Proof of Concept (POC) chatbot designed to answer questions from an uploaded PDF document. This chatbot leverages OpenAI's language models and is developed with **Streamlit** for the user interface. It was fully developed on **GitHub Codespaces** to ensure an easy and seamless setup.
+
+## Key Features:
+- **Interactive UI** built with **Streamlit**, providing a simple and intuitive web interface.
+- **PDF Processing**: Upload a PDF and interact with the chatbot to ask questions based on the document’s content.
+- **AI-Powered**: Uses **OpenAI**'s language models (LLM) to understand and respond to queries.
+
+## Tech Stack:
+- **UI Framework**: Streamlit
+- **Language Model**: OpenAI
+- **Development Environment**: Fully developed using **GitHub Codespaces**.
+
+
+### Project is working in progress...
---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+
+    st.header("Chat with multiple PDFs :books:")
+    st.text_input("Ask a question about your documents:")
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                #get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
+
+                #get the text chunks
+
+                #create vector store
+        
+
+if __name__ == '__main__':
+    main()
