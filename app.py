0aebb9e4ab15b33756890b6bd6ad9b8843544a4c
Shannon Yee <yeesuetee@gmail.com>
2025-06-05 15:46:07 +0000

    Migrate to develop on local

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from transformers import AutoTokenizer, AutoModel
+from InstructorEmbedding import INSTRUCTOR
+from langchain.embeddings import HuggingFaceEmbeddings
+
+# tokenizer = AutoTokenizer.from_pretrained("hkunlp/instructor-xl")
+# model = AutoModel.from_pretrained("hkunlp/instructor-xl")
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -20,6 +28,20 @@ def get_text_chunks(text):
     )
     chunks = text_splitter.split_text(text)
     return chunks
+
+def get_vectorstore(text_chunks):
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+# def get_vectorstore(text_chunks):
+#     instructor_model = INSTRUCTOR("hkunlp/instructor-xl")
+#     embeddings = HuggingFaceEmbeddings(model=instructor_model)
+#     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+#     return vectorstore
+
 
 def main():
     load_dotenv()
@@ -40,9 +62,10 @@ def main():
 
                 #get the text chunks
                 text_chunks = get_text_chunks(raw_text)
-                st.write(text_chunks)
+                # st.write(text_chunks)
 
                 #create vector store
+                vectorstore = get_vectorstore(text_chunks)
         
 
 if __name__ == '__main__':
